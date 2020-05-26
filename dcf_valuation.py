6ba6ba74251db07b58876a56f717191020d9b478
repl.it user <replituser@example.com>
2020-05-26 13:14:08 +0000

    added comments

--- dcf_valuation.py
+++ dcf_valuation.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-
 import matplotlib.pyplot as plt
 import pandas as pd
 import requests
@@ -14,24 +13,31 @@ class YahooFin():
     BASE_URL = 'https://query1.finance.yahoo.com/v10/finance/quoteSummary/'
 
     def __init__(self, ticker):
-        self.ticker = ticker
+      """Initiates the ticker
+          Args:
+            ticker (str): Stock-ticker Ex. 'AAPL'
+      """
+      self.ticker = ticker
 
     def make_request(self, url):
         """Makes a GET request"""
         return requests.get(url)
 
     def get_data(self):
-        return self.make_request(self.url).json()
+      """Returns a json object from a GET request"""
+      return self.make_request(self.url).json()
 
     def data(self):
-        data_temp = self.get_data()
-        try:
-            return data_temp.get('quoteSummary').get("result")
-        except KeyError as e:
-            print("Something went wrong")
+      """Returns query result from json object"""
+      data_temp = self.get_data()
+      try:
+          return data_temp.get('quoteSummary').get("result")
+      except KeyError as e:
+          print("Something went wrong")
 
     def convert_timestamp(self, raw):
-        return datetime.utcfromtimestamp(raw).strftime('%Y-%m-%d')
+      """Converts UNIX-timestamp to YYYY-MM-DD"""
+      return datetime.utcfromtimestamp(raw).strftime('%Y-%m-%d')
 
     def extract_raw(func):
       """Decorator to remove keys from from json data 
@@ -49,24 +55,26 @@ class YahooFin():
       return wrapper_extract_raw
 
     def create_dict(self):
-        balance_sheet = []
-        temp_data = self._dict
-        for d in temp_data:
-            temp_dict = {}
-            for key, value in d.items():
-                if type(value) == dict and 'raw' in value:
-                    v = value['raw']
-                    temp_dict[key] = v
-            balance_sheet.append(temp_dict)
-        return balance_sheet
+      """Creates a dict from extracted data"""
+      balance_sheet = []
+      temp_data = self._dict
+      for d in temp_data:
+          temp_dict = {}
+          for key, value in d.items():
+              if type(value) == dict and 'raw' in value:
+                  v = value['raw']
+                  temp_dict[key] = v
+          balance_sheet.append(temp_dict)
+      return balance_sheet
 
 
     def to_df(self):
-        self._df = pd.DataFrame.from_dict(self.create_dict())
-        for index, row in self._df.iterrows():
-            self._df.loc[index, 'endDate'] = self.convert_timestamp(self._df.at[index, 'endDate'])
-        self._df = self._df.iloc[::-1]
-        return self._df
+      """Creates a pandas Dataframe from dict"""
+      self._df = pd.DataFrame.from_dict(self.create_dict())
+      for index, row in self._df.iterrows():
+          self._df.loc[index, 'endDate'] = self.convert_timestamp(self._df.at[index, 'endDate'])
+      self._df = self._df.iloc[::-1]
+      return self._df
 
 class BalanceSheetQ(YahooFin):
 
@@ -93,11 +101,12 @@ class BalanceSheetQ(YahooFin):
 
   @YahooFin.extract_raw
   def _balance_sheet(self):
-      data = self.data()
-      query =  data[0]
-      balance_sheet_qty = query['balanceSheetHistoryQuarterly']
-      balance_sheet_statements = balance_sheet_qty['balanceSheetStatements']
-      return balance_sheet_statements
+    """Returns a balance sheet statement"""
+    data = self.data()
+    query =  data[0]
+    balance_sheet_qty = query['balanceSheetHistoryQuarterly']
+    balance_sheet_statements = balance_sheet_qty['balanceSheetStatements']
+    return balance_sheet_statements
 
 
 
@@ -126,14 +135,15 @@ class IncomeStatementQ(YahooFin):
 
   @YahooFin.extract_raw
   def _income_statement(self):
-      data = self.data()
-      query =  data[0]
-      income_statement_qty = query['incomeStatementHistoryQuarterly']
-      income_statement_statements = income_statement_qty['incomeStatementHistory']
-      return income_statement_statements
+    """Returns a income statement"""
+    data = self.data()
+    query =  data[0]
+    income_statement_qty = query['incomeStatementHistoryQuarterly']
+    income_statement_statements = income_statement_qty['incomeStatementHistory']
+    return income_statement_statements
 
 
 if __name__ == '__main__':
     data = IncomeStatementQ('INVE-B.ST')
-    print(data.to_df())
-
+    data = data.to_df()
+    data.plot(x='endDate', y='netIncomeApplicableToCommonShares', kind='bar')
